49cbf04dd89a723ff1cf8ecf28348eeec1c31d44
XUZY <topy-123@qq.com>
2025-02-27 11:25:22 +0800

    ZYSectionData增加属性和快捷调用

--- ZYCommonCellData/ZYCommonCellData.h
+++ ZYCommonCellData/ZYCommonCellData.h
@@ -32,8 +32,15 @@ typedef void (^ZYCellDataCallbackKeyValue)(NSString *key, id value);
 @end
 
 @interface ZYSectionData : NSObject
+@property (nonatomic, strong) NSString *title;
 @property (nonatomic, strong) NSMutableArray *rows;
+@property (nonatomic, assign) BOOL selected;
 @property (nonatomic, assign) BOOL isAlone; //是否单独的一个section，即section之间需不需要间距
+@property (nonatomic, strong) ZYSectionData * (^sTitle)(NSString *title);
+@property (nonatomic, strong) ZYSectionData * (^sIsAlone)(BOOL isAlone);
+@property (nonatomic, strong) ZYSectionData * (^sValue)(NSString *key, id value);
+@property (nonatomic, strong) id (^gValue)(NSString *key);
+@property (nonatomic, strong) ZYSectionData * (^sSelected)(BOOL selected);
 @end
 
 NS_ASSUME_NONNULL_END
--- ZYCommonCellData/ZYCommonCellData.m
+++ ZYCommonCellData/ZYCommonCellData.m
@@ -89,13 +89,63 @@
 }
 @end
 
+@interface ZYSectionData ()
+@property (nonatomic, strong, nullable) NSMutableDictionary *valueDic;
+@end
+
 @implementation ZYSectionData
+
+- (instancetype)init
+{
+    self = [super init];
+    if (self) {
+        __weak __typeof(self)weakSelf = self;
+        self.sTitle = ^ZYSectionData * _Nonnull(NSString * _Nonnull title) {
+            __strong __typeof(weakSelf) strongSelf = weakSelf;
+            strongSelf.title = title;
+            return strongSelf;
+        };
+        
+        self.sIsAlone = ^ZYSectionData * _Nonnull(BOOL isAlone) {
+            __strong __typeof(weakSelf) strongSelf = weakSelf;
+            strongSelf.isAlone = isAlone;
+            return strongSelf;
+        };
+        
+        self.sValue = ^ZYSectionData * _Nonnull(NSString * _Nonnull key, id  _Nonnull value) {
+            __strong __typeof(weakSelf) strongSelf = weakSelf;
+            if (key.length > 0 && value) {
+                [strongSelf.valueDic setObject:value forKey:key];
+            }
+            return strongSelf;
+        };
+        
+        self.gValue = ^id _Nonnull(NSString * _Nonnull key) {
+            __strong __typeof(weakSelf) strongSelf = weakSelf;
+            return [strongSelf.valueDic objectForKey:key];
+        };
+        
+        self.sSelected = ^ZYSectionData * _Nonnull(BOOL selected) {
+            __strong __typeof(weakSelf) strongSelf = weakSelf;
+            strongSelf.selected = selected;
+            return strongSelf;
+        };
+    }
+    return self;
+}
 
 - (NSMutableArray *)rows {
     if (!_rows) {
         _rows = [NSMutableArray array];
     }
     return _rows;
+}
+
+- (NSMutableDictionary *)valueDic {
+    if (!_valueDic) {
+        _valueDic = [NSMutableDictionary dictionary];
+    }
+    return _valueDic;
 }
 
 @end
